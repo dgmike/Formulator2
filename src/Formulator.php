5a44fbaf995ab052d0d3605535257e7101bb7567
Michael Granados <michaelgranados@gmail.com>
2012-09-19 00:18:18 -0300

    Documenting something

--- src/Formulator.php
+++ src/Formulator.php
@@ -2,22 +2,54 @@
 
 namespace Formulator;
 use Formulator\Config as Config;
+use Formulator\Form as Form;
+
+require_once __DIR__ . 'Config.php';
 
 /**
  * Formulator Base Class
+ * 
+ * Use the Formulator\Config object to create all configurations for the object
+ * before use this class to render or manipulate the form.
+ * 
+ * @see \Formulator\Config The config of Formulator
  */
 class Formulator
 {
     protected $form;
-    
+
+    /**
+     * Instanciate a new class
+     *
+     * You must pass an Formulator Config object
+     *
+     * <pre>
+     * $config = array(
+     *     'form' => array(
+     *         'method' => 'post',
+     *         'action' => '/'
+     *     ),
+     * );
+     * $configObject = new \Formulator\Config($config)
+     * $formulatorObject = new \Formulator\Formulator($configObject);
+     * echo $formulatorObject->renderOpenForm();
+     * </pre>
+     * 
+     * @param \Formulator\Config $config the configuration of form
+     */
 	public function __construct(Config $config = null)
 	{
-	    $this->form = new \Formulator\Form;
+	    $this->form = new Form;
 	    if ($config) {
     		$this->config($config);
         }
 	}
 
+    /**
+     * Configures the Formulator Object
+     * 
+     * You need to pass an new Config object
+     */
     public function config(Config $config)
     {
         if ($config->hasElements()) {
